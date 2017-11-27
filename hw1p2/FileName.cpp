e93034de4e949e84adfe2f108fdf58ffa6c98465
stdio2016 <randomfrank8@gmail.com>
2017-11-27 22:21:35 +0800

    park

--- hw1p2/FileName.cpp
+++ hw1p2/FileName.cpp
@@ -1,7 +1,7 @@
 #include "SceneManager.h"
 
 // constants
-const std::string SceneManager::DefaultCameraFileName = "Chess/Chess.view";
-const std::string SceneManager::DefaultLightFileName = "Chess/Chess.light";
-const std::string SceneManager::DefaultSceneFileName = "Chess/Chess.scene";
-const std::string SceneManager::DefaultModelFolder = "Chess/";
+const std::string SceneManager::DefaultCameraFileName = "Park/Park.view";
+const std::string SceneManager::DefaultLightFileName = "Park/Park.light";
+const std::string SceneManager::DefaultSceneFileName = "Park/Park.scene";
+const std::string SceneManager::DefaultModelFolder = "Park/";
